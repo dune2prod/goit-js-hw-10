0f4e535ae60ed06782f6b0996d87dc38e9863bc9
dune2prod <122452986+dune2prod@users.noreply.github.com>
2023-06-20 09:24:35 +0300

    111

---
+++ src/index.js
@@ -0,0 +1,93 @@
+// Прогноз погоды**********************************************************************************************************
+// const search = document.querySelector('.js-search');
+// const list = document.querySelector('.js-list');
+// search.addEventListener('submit', onSearch);
+
+// function onSearch(evt) {
+//     evt.preventDefault()
+
+//     const {query, days } = evt.currentTarget.elements
+//     getWeather(query.value, days.value)
+//         .then((data) => (list.innerHTML = createMarkup(data.forecast.forecastday)))
+//         .catch((err) => console.log(err));
+// }
+
+// function getWeather(city, days) {
+//     const BASE_URL = 'http://api.weatherapi.com/v1';
+//     const API_KEY = '711f26cba4e74e7492673913231706';
+
+//     return fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}&lang=uk`)
+//         .then(resp => {
+//             if (!resp.ok) {
+//                 throw new Error(resp.statusText);
+//             }
+            
+//             return resp.json()
+//         });
+// }
+
+
+// function createMarkup(arr) {
+    
+//     return arr.map(({ date, day: { avgtemp_c, condition: { icon, text } } }) =>  `<li>
+//     <img src="${icon}" alt="${text}">
+//     <p>${text}</p>
+//     <h2>${date}</h2>
+//     <h3>${avgtemp_c}</h3>
+//   </li>`).join('')
+// }
+// Котики*********************************************************************************************
+// import ABC from "./cat-api.js";
+// import {a} from "./cat-api.js";
+
+
+const catSelector = document.querySelector('.breed-select');
+const catInfo = document.querySelector(".cat-info");
+catSelector.addEventListener('change', onInputSelect);
+
+const API_KEY = 'api_key=live_FmoyFNMFk66lp5FlkqDMo2DsUUF779eEfwbDSw2SDerzlMDn8v9p7wHPQp65RBcv';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+function onInputSelect(event) {
+    console.log(event.target.value)
+    return fetch(`${BASE_URL}/images/search?breed_ids=${event.target.value}&${API_KEY}`)
+        .then(Response => {
+            if (!Response.ok) {
+            throw new Error(Response.statusText)
+            }
+           
+           return Response.json();
+        })
+        .then(data => console.log(data[0].breeds[0]))
+        
+    .catch((err) => console.log(err))
+};
+
+function fetchBreeds() {
+    
+    
+       return fetch(`${BASE_URL}/breeds?${API_KEY}`)
+        .then(Response => {
+            if (!Response.ok) {
+                throw new Error(Response.statusText)
+            }
+            
+           return Response.json()
+        
+        })
+           
+    .catch((err) => console.log(err))
+};
+
+function createCatlistMarkup(arr) {
+    const markup = [];
+     arr.map(({ id, name }) => {
+        markup.push(`<option value="${id}">${name}</option>`)
+          
+     })
+       catSelector.innerHTML = markup.join()
+};
+
+
+fetchBreeds()
+.then(data => createCatlistMarkup(data))
